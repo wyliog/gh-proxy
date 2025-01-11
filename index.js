10ea731b56ad0d1ae74536ec6fdbaf6af6956bf0
wyliog <wyliog@163.com>
2025-01-11 18:29:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://hunshcn.github.io/gh-proxy/'
+const ASSET_URL = 'https://proxy.55zs.today/g/'
 // 前缀，如果自定义路由为example.com/gh/*，将PREFIX改为 '/gh/'，注意，少一个杠都会错！
-const PREFIX = '/'
+const PREFIX = '/g/'
 // 分支文件使用jsDelivr镜像的开关，0为关闭，默认关闭
 const Config = {
     jsdelivr: 0
